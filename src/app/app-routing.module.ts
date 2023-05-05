82a79d2148ac80b21a857d06948364862b8c4dff
Daniel Kurosz <danielkurosz@gmail.com>
2023-05-05 22:12:06 +0200

    Init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'chat', pathMatch: 'full' },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) },
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: '**', redirectTo: 'chat' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
