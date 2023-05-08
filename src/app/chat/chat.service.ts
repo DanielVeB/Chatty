b2daf81ffb18ade3ee4798b6f1c1e9a3ace636f2
Daniel Kurosz <danielkurosz@gmail.com>
2023-05-08 22:56:46 +0200

    update content css

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -32,7 +32,58 @@ export class ChatService {
       author: Author.Bot,
       text: "The price depends on the product you're interested in. Could you tell me which product you're considering?",
       date: new Date()
+    },
+    {
+      author: Author.Bot,
+      text: "Hi there! I'm a chat bot. How can I assist you today?",
+      date: new Date()
+    },
+    {
+      author: Author.User,
+      text: "Hi, I have a question about your product",
+      date: new Date()
+    },
+    {
+      author: Author.Bot,
+      text: "Sure thing! What would you like to know?",
+      date: new Date()
+    },
+    {
+      author: Author.User,
+      text: "How much does it cost?",
+      date: new Date()
+    },
+    {
+      author: Author.Bot,
+      text: "The price depends on the product you're interested in. Could you tell me which product you're considering?",
+      date: new Date()
+    },
+    {
+      author: Author.Bot,
+      text: "Hi there! I'm a chat bot. How can I assist you today?",
+      date: new Date()
+    },
+    {
+      author: Author.User,
+      text: "Hi, I have a question about your product",
+      date: new Date()
+    },
+    {
+      author: Author.Bot,
+      text: "Sure thing! What would you like to know?",
+      date: new Date()
+    },
+    {
+      author: Author.User,
+      text: "How much does it cost?",
+      date: new Date()
+    },
+    {
+      author: Author.Bot,
+      text: "The price depends on the product you're interested in. Could you tell me which product you're considering?",
+      date: new Date()
     }
+    
   ];
 
   constructor() { }
